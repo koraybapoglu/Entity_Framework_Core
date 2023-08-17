526cfea7e65be3b2823003c507398872a405e2b7
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-18 00:21:54 +0300

    commit

---
+++ Database_First/PMC-DOTNET CLI Ekstra Bilgiler/Force_Kullanimina_Karsi_Partial_Kullanimi/Program.cs
@@ -0,0 +1,13 @@
+﻿using Entity_DatabaseFirst.Models;
+
+namespace Entity_DatabaseFirst
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Category category = new Category();
+			category.ContextContextContext = 0;
+		}
+	}
+}
