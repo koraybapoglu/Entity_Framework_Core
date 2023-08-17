279f22b15e5df14bda4eb73d258da8e82d6c651d
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-17 22:41:50 +0300

    commit

---
+++ Database_First/DOTNET CLI Kullanımı ve Örnek Proje/Ornek/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Entity_DatabaseFirst
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+		public void Void2() 
+		{
+
+		}
+	}
+}
