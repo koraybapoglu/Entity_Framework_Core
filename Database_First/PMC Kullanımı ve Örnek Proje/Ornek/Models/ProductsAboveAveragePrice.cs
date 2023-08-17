5a2317bc29ddc9affe7e5c3745fa3e31c3416105
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-17 19:57:53 +0300

    commit

---
+++ Database_First/PMC Kullanımı ve Örnek Proje/Ornek/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entity_DatabaseFirst.Models;
+
+public partial class ProductsAboveAveragePrice
+{
+    public string ProductName { get; set; } = null!;
+
+    public decimal? UnitPrice { get; set; }
+}
