835d69d5b4c0afbb7e6baee9fba9590a52fb5e5d
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-18 00:24:39 +0300

    Create Models

---
+++ Database_First/PMC-DOTNET CLI Ekstra Bilgiler/Force_Kullanimina_Karsi_Partial_Kullanimi/Models/Partial/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_DatabaseFirst.Models;
+
+public partial class Category //PARTİAL KULLANARAK CATEGORY ENTİTY'SİNDE YAPACAĞIMIZ DEĞİŞİKLİKLERİ -FORCE KULLANSAK BİLE SİLİNMİCEK ŞEKİLDE TASARLADIK.
+{
+	public int ContextContextContext { get; set; }
+}
+
