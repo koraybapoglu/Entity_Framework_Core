1c8ffc33b2f820d66734f4886ca417f54e0915d0
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-23 16:05:33 +0300

    Create 20230823123509_AddYorumlarTable.cs

---
+++ Code_First/Migrations/20230823123509_AddYorumlarTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddYorumlarTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Yorumlar",
+                columns: table => new
+                {
+                    YorumID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KitapID = table.Column<int>(type: "int", nullable: false),
+                    MusteriID = table.Column<int>(type: "int", nullable: false),
+                    YorumMetni = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YorumTarihi = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Yorumlar", x => x.YorumID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Yorumlar");
+        }
+    }
+}
