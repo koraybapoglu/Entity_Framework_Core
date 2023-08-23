24cb7655f74c55dbb851c2495cc16f6f22af9016
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-23 16:04:39 +0300

    Create 20230823123040_AddKategori.cs

---
+++ Code_First/Migrations/20230823123040_AddKategori.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKategori : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kategoriler",
+                columns: table => new
+                {
+                    KategoriID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KategoriAdi = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kategoriler", x => x.KategoriID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kategoriler");
+        }
+    }
+}
