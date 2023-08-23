39f34cc029e1b57ea80e896250ba36daa764b68e
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-23 23:23:36 +0300

    Create 20230823190310_Init.cs

---
+++ Entity_VeriGuncelleme/Migrations/20230823190310_Init.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_VeriGuncelleme.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Uyeler",
+                columns: table => new
+                {
+                    UyeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UyeAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UyeSoyadi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UyeYasi = table.Column<int>(type: "int", nullable: false),
+                    UyeCinsiyet = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Uyeler", x => x.UyeID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Uyeler");
+        }
+    }
+}
