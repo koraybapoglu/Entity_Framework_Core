1a30c9e1dff8c00acca567128209dc291ae2f2c8
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-23 16:04:17 +0300

    Create 20230823122445_mig_1.cs

---
+++ Code_First/Migrations/20230823122445_mig_1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kitaplar",
+                columns: table => new
+                {
+                    KitapID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KitapBaslik = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    KitapYayinTarihi = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kitaplar", x => x.KitapID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Musteriler",
+                columns: table => new
+                {
+                    MusteriID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MusteriAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MusteriSoyadi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MusteriEposta = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musteriler", x => x.MusteriID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Yazarlar",
+                columns: table => new
+                {
+                    YazarID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    YazarAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YazarSoyadi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YazarDogumTarihi = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Yazarlar", x => x.YazarID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kitaplar");
+
+            migrationBuilder.DropTable(
+                name: "Musteriler");
+
+            migrationBuilder.DropTable(
+                name: "Yazarlar");
+        }
+    }
+}
