f2ba343031b372beeae9af345ef416dc2950a8fe
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-23 21:01:32 +0300

    commit

---
+++ Entity_VeriEkleme/Migrations/20230823173700_İnit.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_VeriEkleme.Migrations
+{
+    /// <inheritdoc />
+    public partial class İnit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Uyeler",
+                columns: table => new
+                {
+                    UyeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UyeAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UyeSoyadi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UyeCinsiyeti = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Uyeler", x => x.UyeID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Uyeler");
+        }
+    }
+}
