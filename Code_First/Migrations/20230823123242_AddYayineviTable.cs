fa7d31ca6db7cb81c9a93498bf325a9c269f22a4
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-23 16:05:07 +0300

    Create 20230823123242_AddYayineviTable.cs

---
+++ Code_First/Migrations/20230823123242_AddYayineviTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddYayineviTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "YayinEvleri",
+                columns: table => new
+                {
+                    YayinEviID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    YayinEviAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    KurulusTarihi = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_YayinEvleri", x => x.YayinEviID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "YayinEvleri");
+        }
+    }
+}
