968121b368a8db19f2d98147baac1d27cb4cb924
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-24 17:16:23 +0300

    Create 20230824140704_UpdateUrunFiyatInt.cs

---
+++ Entity_VeriSilme/Migrations/20230824140704_UpdateUrunFiyatInt.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Veri_Silme.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUrunFiyatInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UrunFiyat",
+                table: "Urunler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UrunFiyat",
+                table: "Urunler",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
