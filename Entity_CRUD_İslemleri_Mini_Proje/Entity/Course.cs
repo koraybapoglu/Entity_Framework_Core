096c1a073150f96a90d0da7262e8ad1905ce4489
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-24 18:46:48 +0300

    Create Course.cs

---
+++ Entity_CRUD_İslemleri_Mini_Proje/Entity/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_CRUD_Islemleri.Entity
+{
+	class Course
+	{
+		public int CourseID { get; set; }
+		public string Title { get; set; }
+	}
+}
