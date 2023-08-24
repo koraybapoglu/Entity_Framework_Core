4be9261953de851198069038607ac315f9e86861
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-24 18:46:43 +0300

    Create Student.cs

---
+++ Entity_CRUD_İslemleri_Mini_Proje/Entity/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_CRUD_Islemleri.Entity
+{
+	class Student
+	{
+		public int StudentID { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+	}
+}
