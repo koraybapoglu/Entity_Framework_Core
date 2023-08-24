c19b70412ac400e4a752c4909536c2c8abfd544b
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-24 18:47:10 +0300

    Create SchoolDbContext.cs

---
+++ Entity_CRUD_İslemleri_Mini_Proje/DbContext/SchoolDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity_CRUD_Islemleri.Entity;
+
+namespace Entity_CRUD_Islemleri
+{
+	class SchoolDbContext : DbContext
+	{
+		public DbSet<Student> Students { get; set; }
+		public DbSet<Course> Courses { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-RN1V6Q9;Database=School;Trusted_Connection=True;TrustServerCertificate=true;");
+		}
+	}
+}
