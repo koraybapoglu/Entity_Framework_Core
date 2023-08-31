c7f548b29d7df5e30b7746b61a74eab035ea30b3
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-31 23:45:44 +0300

    Create Parca.cs

---
+++ Entity_VeriSorgulama_Teknikleri/Count,LongCount,Any,Max,Min,Distinct,All,Sum,Average,Like Query/Entities/Parca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_VeriGetirme.Entities
+{
+	public class Parca
+	{
+		public int ParcaID { get; set; }
+		public string ParcaAdi { get; set; }
+	}
+}
