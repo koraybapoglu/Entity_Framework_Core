131f95cbd1ee7e5b3fb201a9cc60d0c54e5e36ce
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-31 23:45:22 +0300

    Create ETicaretDbContext.cs

---
+++ Entity_VeriSorgulama_Teknikleri/Count,LongCount,Any,Max,Min,Distinct,All,Sum,Average,Like Query/DbContext/ETicaretDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Entity_VeriGetirme.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_VeriGetirme
+{
+	public class ETicaretDbContext : DbContext
+	{
+		public DbSet<Urun> Urunler { get; set; }
+		public DbSet<Parca> Parcalar { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+
+			optionsBuilder.UseSqlServer("Server=DESKTOP-RN1V6Q9;Database=ETicaretDB;Trusted_Connection=True;TrustServerCertificate=true;");
+		}
+	}
+}
