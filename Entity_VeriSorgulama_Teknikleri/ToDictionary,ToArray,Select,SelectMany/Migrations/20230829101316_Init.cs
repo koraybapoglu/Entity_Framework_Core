98129cd2bb0360b88774528b2ee9dc4e98a0b21d
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-01 21:55:50 +0300

    Create 20230829101316_Init.cs

---
+++ Entity_VeriSorgulama_Teknikleri/ToDictionary,ToArray,Select,SelectMany/Migrations/20230829101316_Init.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_VeriGetirme.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urunler",
+                columns: table => new
+                {
+                    UrunID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UrunAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fiyat = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urunler", x => x.UrunID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parcalar",
+                columns: table => new
+                {
+                    ParcaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParcaAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrunID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parcalar", x => x.ParcaID);
+                    table.ForeignKey(
+                        name: "FK_Parcalar_Urunler_UrunID",
+                        column: x => x.UrunID,
+                        principalTable: "Urunler",
+                        principalColumn: "UrunID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parcalar_UrunID",
+                table: "Parcalar",
+                column: "UrunID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parcalar");
+
+            migrationBuilder.DropTable(
+                name: "Urunler");
+        }
+    }
+}
